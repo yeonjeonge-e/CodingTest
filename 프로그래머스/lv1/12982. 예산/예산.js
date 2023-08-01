32f852a9304adafc757d075a3827d3ed7cd76e08
yeonjeong.choi <62794884+yeonjeonge-e@users.noreply.github.com>
2023-08-02 04:25:49 +0900

    [level 1] Title: 예산, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -0,0 +1,28 @@
+function solution(d, budget) {
+    var answer = 0;
+    let sum = 0;
+    
+//    let a = d.join();
+    // console.log(a);              1,3,2,5,4
+//    let b = d.sort();
+    // console.log(b);              [ 1, 2, 3, 4, 5 ]
+    
+    d.sort((a, b) => a - b);
+    
+    for (let i=0; i<d.length; i++) {
+        sum += d[i];
+        // console.log(sum);        1 3 6 10 15
+        if (sum <= budget) {
+            answer ++;
+        } else {
+            return answer;
+        }
+    }
+    return answer;
+}
+
+
+
+// d.sort((a, b) => a - b)를 사용해 보세요.
+
+// d.sort()를 사용하면 [1, 2, 10, 30]가 [1, 10, 2, 30]로 됩니다.
