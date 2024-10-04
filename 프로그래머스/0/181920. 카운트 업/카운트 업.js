cce0070b79b7471588ea22dd204a8b4c738035d4
duswjd_e <62794884+yeonjeonge-e@users.noreply.github.com>
2024-10-04 23:59:47 +0900

    [level 0] Title: 카운트 업, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.js
@@ -0,0 +1,17 @@
+// function solution(start_num, end_num) {
+//     let list = [];
+//     for (let i = start_num; i <= end_num, i++) {
+//         list.push(i);
+//     }
+//     return list;
+// }
+
+function solution(start_num, end_num) {
+  let list =[]
+  let num;
+  for (num=start_num; num<=end_num; num++) {
+   list.push(num)
+     
+   }
+return list;
+}
